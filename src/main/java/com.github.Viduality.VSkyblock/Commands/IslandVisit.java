537874019454a3601b4f9eb4985c0204bd46a8e2
Viduality <viduality@users.noreply.github.com>
2020-03-19 20:56:18 +0100

    Fixed /island visit command

--- src/main/java/com.github.Viduality.VSkyblock/Commands/IslandVisit.java
+++ src/main/java/com.github.Viduality.VSkyblock/Commands/IslandVisit.java
@@ -43,25 +43,29 @@ public class IslandVisit implements SubCommand {
                                                 public void onQueryDone(boolean result) {
                                                     if (result) {
                                                         String island = "VSkyblockIsland_" + islandid;
-                                                        if (!wm.getSpawnLocation(island).getBlock().getRelative(BlockFace.DOWN).getType().equals(Material.AIR)) {
-                                                            if (wm.getSpawnLocation(island).getBlock().getType().equals(Material.AIR)) {
-                                                                if (wm.getSpawnLocation(island).getBlock().getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
-                                                                    if (!wm.getSpawnLocation(island).getBlock().getRelative(BlockFace.DOWN).getType().equals(Material.LAVA)) {
-                                                                        if (!wm.getSpawnLocation(island).getBlock().getRelative(BlockFace.DOWN).getType().equals(Material.MAGMA_BLOCK)) {
-                                                                            if (!wm.getSpawnLocation(island).getBlock().getRelative(BlockFace.DOWN).getType().equals(Material.WITHER_ROSE)) {
-                                                                                player.teleport(wm.getSpawnLocation(island));
-                                                                                databaseReader.getIslandMembers(islandid, new DatabaseReader.CallbackList() {
-                                                                                    @Override
-                                                                                    public void onQueryDone(List<String> result) {
-                                                                                        for (String member : result) {
-                                                                                            OfflinePlayer offlinePlayer = plugin.getServer().getOfflinePlayer(member);
-                                                                                            if (offlinePlayer.isOnline()) {
-                                                                                                Player onlinePlayer = (Player) offlinePlayer;
-                                                                                                ConfigShorts.custommessagefromString("PlayerVisitingYourIsland", onlinePlayer, player.getName());
+                                                        if (wm.getLoadedWorlds().contains(island)) {
+                                                            if (wm.getSpawnLocation(island).getBlock().getRelative(BlockFace.DOWN).getType() != Material.AIR) {
+                                                                if (wm.getSpawnLocation(island).getBlock().getType().equals(Material.AIR)) {
+                                                                    if (wm.getSpawnLocation(island).getBlock().getRelative(BlockFace.UP).getType().equals(Material.AIR)) {
+                                                                        if (!wm.getSpawnLocation(island).getBlock().getRelative(BlockFace.DOWN).getType().equals(Material.LAVA)) {
+                                                                            if (!wm.getSpawnLocation(island).getBlock().getRelative(BlockFace.DOWN).getType().equals(Material.MAGMA_BLOCK)) {
+                                                                                if (!wm.getSpawnLocation(island).getBlock().getRelative(BlockFace.DOWN).getType().equals(Material.WITHER_ROSE)) {
+                                                                                    player.teleport(wm.getSpawnLocation(island));
+                                                                                    databaseReader.getIslandMembers(islandid, new DatabaseReader.CallbackList() {
+                                                                                        @Override
+                                                                                        public void onQueryDone(List<String> result) {
+                                                                                            for (String member : result) {
+                                                                                                OfflinePlayer offlinePlayer = plugin.getServer().getOfflinePlayer(member);
+                                                                                                if (offlinePlayer.isOnline()) {
+                                                                                                    Player onlinePlayer = (Player) offlinePlayer;
+                                                                                                    ConfigShorts.custommessagefromString("PlayerVisitingYourIsland", onlinePlayer, player.getName());
+                                                                                                }
                                                                                             }
                                                                                         }
-                                                                                    }
-                                                                                });
+                                                                                    });
+                                                                                } else {
+                                                                                    ConfigShorts.messagefromString("IslandSpawnNotSafe", player);
+                                                                                }
                                                                             } else {
                                                                                 ConfigShorts.messagefromString("IslandSpawnNotSafe", player);
                                                                             }
