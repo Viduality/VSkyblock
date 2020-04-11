da18d389cf84c4759e86d8c98521a0473fcfc5ff
Viduality <viduality@users.noreply.github.com>
2020-04-11 14:35:34 +0200

    Blocks coarse dirt to dirt conversion.

--- src/main/java/com.github.Viduality.VSkyblock/Listener/InteractBlocker.java
+++ src/main/java/com.github.Viduality.VSkyblock/Listener/InteractBlocker.java
@@ -3,7 +3,9 @@ package com.github.Viduality.VSkyblock.Listener;
 import com.github.Viduality.VSkyblock.Commands.Island;
 import com.github.Viduality.VSkyblock.Utilitys.ConfigShorts;
 import com.github.Viduality.VSkyblock.VSkyblock;
+import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,6 +13,9 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+
+import java.util.EnumSet;
+import java.util.Set;
 
 public class InteractBlocker implements Listener {
 
@@ -54,4 +59,27 @@ public class InteractBlocker implements Listener {
             event.setCancelled(true);
         }
     }
+
+    @EventHandler
+    public void interactAtCoarseDirt(PlayerInteractEvent playerInteractEvent) {
+        Player player = playerInteractEvent.getPlayer();
+        if (playerInteractEvent.getClickedBlock() != null) {
+            if (playerInteractEvent.getClickedBlock().getType().equals(Material.COARSE_DIRT)
+                    && playerInteractEvent.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+                if (hoeTool.contains(playerInteractEvent.getPlayer().getInventory().getItemInMainHand().getType())
+                        || hoeTool.contains(playerInteractEvent.getPlayer().getInventory().getItemInOffHand().getType())) {
+                    playerInteractEvent.setCancelled(true);
+                }
+            }
+        }
+    }
+
+
+    private static final Set<Material> hoeTool = EnumSet.of(
+            Material.WOODEN_HOE,
+            Material.STONE_HOE,
+            Material.IRON_HOE,
+            Material.GOLDEN_HOE,
+            Material.DIAMOND_HOE
+    );
 }
